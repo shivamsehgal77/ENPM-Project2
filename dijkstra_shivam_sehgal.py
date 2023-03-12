7d7f7414350339eded590645b051d2d8cc952c4a
Shivam <ssehgal7@umd.edu>
2023-03-11 23:39:24 -0500

    fifth commit

--- dijkstra_shivam_sehgal.py
+++ dijkstra_shivam_sehgal.py
@@ -1,7 +1,6 @@
 import pygame
 import sys
-import matplotlib.pyplot as plt
-import cv2
+
 from queue import PriorityQueue
 
 
@@ -18,92 +17,114 @@ green = (0,255,0)
 points_triangle = [(460, 25), (460, 225), (510, 125)]
 points_hexgon = [(300,44.22649731),(230.04809472,84.61324865),(230.04809472,165.38675135),(300,205.77350269),(369.95190528,165.38675135),(369.95190528,84.61324865)]
 points_triangle_border = [(455,246.18033989),(455.00,3.81966011),(515.59016994,125)]
-# points_triangle_border = 
+
 # Set the dimensions of the screen
-screen = pygame.display.set_mode((600, 250))
+# screen = pygame.display.set_mode((600, 250))
+# screen = pygame.display.set_mode((600, 250), pygame.NOFRAME)
+screen = pygame.Surface((600, 250))
 pygame.draw.rect(screen, white, pygame.Rect(5,5,590,240))
 pygame.draw.rect(screen,red,pygame.Rect(95,5,60,105))
 pygame.draw.rect(screen, red, pygame.Rect(100, 5, 50, 100))
 pygame.draw.rect(screen, red, pygame.Rect(95, 140, 60, 105))
 pygame.draw.rect(screen, red, pygame.Rect(100, 145, 50, 100))
-# pygame.draw.polygon(screen, red, points_triangle_border)
+pygame.draw.polygon(screen, red, points_triangle_border)
 pygame.draw.polygon(screen, red, points_triangle)
 pygame.draw.polygon(screen, red, points_hexgon)
 pixel_color = (0, 0,255) # Red
-pixel_x = 0
-pixel_y = 300
 
-
+# Move the robot up
 def robot_move_up(robot):
     x,y=robot
     robot=(x,y-1)
     cost=1
     return cost,robot
-
+# Move the robot down
 def robot_move_down(robot):
     x,y=robot
     robot=(x,y+1)
     cost=1
     return cost,robot
+# Move the robot left
 def robot_move_left(robot):
     x,y=robot
     robot=(x-1,y)
     cost=1
     return cost,robot
+# Move the robot right
 def robot_move_right(robot):
     x,y=robot
     robot=(x+1,y)
     cost=1
     return cost,robot
-
-
+# Move the robot up left
 def robot_move_up_left(robot):
     x,y=robot
     robot=(x-1,y-1)
     cost=1.4
     return cost,robot
+# Move the robot up right
 def robot_move_up_right(robot):
     x,y=robot
     robot=(x+1,y-1)
     cost=1.4
     return cost,robot
-
+# Move the robot down left
 def robot_move_down_left(robot):
     x,y=robot
     robot=(x-1,y+1)
     cost=1.4
     return cost,robot
-
+# Move thr robot down right
 def robot_move_down_right(robot):
     x,y=robot
     robot=(x+1,y+1)
     cost=1.4
     return cost,robot
 
-black = (0, 0, 0)
+blue = (0, 0, 255)
 
+# Takes the input for start and goal node takes only correct input if bad inputs asks again
+while True:
+    try:
+        start_x=int(input("Enter the starting x coordinate \n"))    
+        start_y=int(input("Enter the starting ycoordinate \n"))    
+        goal_x=int(input("Enter the starting x coordinate \n"))    
+        goal_y=int(input("Enter the starting ycoordinate \n"))    
+        robot_start_position=(start_x,250-start_y)
+        robot_goal_position=(goal_x,250-goal_y)
+        if screen.get_at(robot_start_position) != white and screen.get_at(robot_goal_position)!=white:
+            raise ValueError
+        break
+    except ValueError:
+        print("Wrong input entered. Please enter an integer in correct range x(0,599) and y(0,249).")
     
 
-robot_start_position=(10,10)
-robot_goal_position=(50,90)
+
 # robot_goal_position=(550,30)
-ctc_node=0
-parent_node_index=None
-node_index=0
-closed_list={}
-open_list=PriorityQueue()
-info=[ctc_node,node_index,parent_node_index,robot_start_position]
+
+ctc_node=0  # cost to come for start node
+parent_node_index=None # Index for the parent node
+node_index=0 # Index of the current node
+closed_list={} # list to store information about the current node
+open_list=PriorityQueue() # list the store nodes and pop them according to priority
+info=[ctc_node,node_index,parent_node_index,robot_start_position] # list to save all info of a node
 open_list.put(info)
+
 while True:
+    # if the open list is empty means that no solution could be found
     if(open_list.empty()):
         print("No solution")
+
     info=open_list.get()
+    # if the goal positoin is reached 
     if(info[3]==robot_goal_position):
         print("goalt reached")
         closed_list[info[1]]=[info[0],info[2],info[3]]
         print(closed_list[info[2]])
         goal_node=info[1]
         break
+
+    # making the node for the new postion and adding to priority queue
     ctc_move,new_pos=robot_move_left(info[3])
     if(new_pos==robot_goal_position):
         print("goal reached")
@@ -121,12 +142,9 @@ while True:
                 index = next(i for i, [_, _, _, (x, y)] in enumerate(open_list.queue) if (x, y) == new_pos)
                 if round(open_list.queue[index][0], 1) > round(ctc_move + info[0], 1):
                     open_list.queue[index][0] = ctc_move + info[0]
-                    open_list.queue[index][2] = info[1]
+                    open_list.queue[index][2] = info[1]              
 
-                            
-                    
-
-
+    # making the node for the new postion and adding to priority queue
     ctc_move,new_pos=robot_move_up(info[3])
     if(new_pos==robot_goal_position):
         print("goal reached")
@@ -146,7 +164,7 @@ while True:
                     open_list.queue[index][0] = ctc_move + info[0]
                     open_list.queue[index][2] = info[1]
                             
-                            
+    # making the node for the new postion and adding to priority queue                        
     ctc_move,new_pos=robot_move_right(info[3])
     if(new_pos==robot_goal_position):
         print("goal reached")
@@ -167,7 +185,7 @@ while True:
                     open_list.queue[index][2] = info[1]
                             
                             
-
+    # making the node for the new postion and adding to priority queue  
     ctc_move,new_pos=robot_move_down(info[3])
     if(new_pos==robot_goal_position):
         print("goal reached")
@@ -187,7 +205,7 @@ while True:
                     open_list.queue[index][0] = ctc_move + info[0]
                     open_list.queue[index][2] = info[1]
                             
-    
+    # making the node for the new postion and adding to priority queue
     ctc_move,new_pos=robot_move_down_left(info[3])
     if(new_pos==robot_goal_position):
         print("goal reached")
@@ -207,7 +225,7 @@ while True:
                     open_list.queue[index][0] = ctc_move + info[0]
                     open_list.queue[index][2] = info[1]
                             
-    
+    # making the node for the new postion and adding to priority queue
     ctc_move,new_pos=robot_move_down_right(info[3])
     if(new_pos==robot_goal_position):
         print("goal reached")
@@ -227,7 +245,7 @@ while True:
                     open_list.queue[index][0] = ctc_move + info[0]
                     open_list.queue[index][2] = info[1]
                             
-    
+    # making the node for the new postion and adding to priority queue
     ctc_move,new_pos=robot_move_up_left(info[3])
     if(new_pos==robot_goal_position):
         print("goal reached")
@@ -247,7 +265,7 @@ while True:
                     open_list.queue[index][0] = ctc_move + info[0]
                     open_list.queue[index][2] = info[1]
                             
-
+    # making the node for the new postion and adding to priority queue
     ctc_move,new_pos=robot_move_up_right(info[3])
     if(new_pos==robot_goal_position):
         print("goal reached")
@@ -267,39 +285,41 @@ while True:
                     open_list.queue[index][0] = ctc_move + info[0]
                     open_list.queue[index][2] = info[1]
                             
-                                                    
+    # append the node to node list                                               
     closed_list[info[1]]=[info[0],info[2],info[3]]
-    screen.set_at(info[3], black)
-    pygame.display.update()
-    print(info[2])
-
+    print(f"Closed_list{info[1]}]",closed_list[info[1]])
+    # screen.set_at(info[3], black)
     
 
 
+# Create a screen
+screen_display = pygame.display.set_mode((600, 250))
 
-print(closed_list)
+# Display the surface on the screen
+screen_display.blit(screen, (0, 0))
+for values in closed_list.values():
+    screen_display.set_at(values[2], blue)
+# Update the screen to show the changes
+    pygame.display.update()
 
-
-# def backtrack():
+# Find the path form start to goal
 path = []
 current_node = goal_node
 while goal_node is not None:
     goal_node_parent = closed_list[goal_node][1]
     path.append(closed_list[goal_node][2])
-    screen.set_at(closed_list[goal_node][2], green)
+    screen_display.set_at(closed_list[goal_node][2], green)
     goal_node=goal_node_parent
     pygame.display.update()
-    
-
-    
     
 
 
 # reverse the path list to get the correct order of nodes
 path.reverse()
-print(path)
+print("****** The optimum path is ****",path)
 
 
+# opens the screen
 while True:
     # Event loop to handle user inputs
     for event in pygame.event.get():
@@ -308,12 +328,3 @@ while True:
             sys.exit()
 
             
-            
-
-
-    # Update the screen
-    
-
-
-
-
