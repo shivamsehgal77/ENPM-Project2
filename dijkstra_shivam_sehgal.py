4edf32a9480d43dc81b939c917a6fbc1f33c9ec1
Shivam <ssehgal7@umd.edu>
2023-03-11 20:33:25 -0500

    4th commit

--- dijkstra_shivam_sehgal.py
+++ dijkstra_shivam_sehgal.py
@@ -3,7 +3,7 @@ import sys
 import matplotlib.pyplot as plt
 import cv2
 from queue import PriorityQueue
-import functions
+
 
 # Initialize Pygame
 pygame.init()
@@ -13,7 +13,7 @@ white=(255,255,255)
 # Initializing Color
 
 red = (255,0,0)
-green = (255,0,0)
+green = (0,255,0)
 # points_hexgon = [(443.30, 125),(421.65, 164.33),(378.35, 164.33),(356.70, 125),(378.35, 85.67),(421.65, 85.67)]
 points_triangle = [(460, 25), (460, 225), (510, 125)]
 points_hexgon = [(300,44.22649731),(230.04809472,84.61324865),(230.04809472,165.38675135),(300,205.77350269),(369.95190528,165.38675135),(369.95190528,84.61324865)]
@@ -22,11 +22,11 @@ points_triangle_border = [(455,246.18033989),(455.00,3.81966011),(515.59016994,1
 # Set the dimensions of the screen
 screen = pygame.display.set_mode((600, 250))
 pygame.draw.rect(screen, white, pygame.Rect(5,5,590,240))
-pygame.draw.rect(screen,green,pygame.Rect(95,5,60,105))
+pygame.draw.rect(screen,red,pygame.Rect(95,5,60,105))
 pygame.draw.rect(screen, red, pygame.Rect(100, 5, 50, 100))
-pygame.draw.rect(screen, green, pygame.Rect(95, 140, 60, 105))
+pygame.draw.rect(screen, red, pygame.Rect(95, 140, 60, 105))
 pygame.draw.rect(screen, red, pygame.Rect(100, 145, 50, 100))
-# pygame.draw.polygon(screen, green, points_triangle_border)
+# pygame.draw.polygon(screen, red, points_triangle_border)
 pygame.draw.polygon(screen, red, points_triangle)
 pygame.draw.polygon(screen, red, points_hexgon)
 pixel_color = (0, 0,255) # Red
@@ -85,7 +85,8 @@ black = (0, 0, 0)
     
 
 robot_start_position=(10,10)
-robot_goal_position=(550,30)
+robot_goal_position=(50,90)
+# robot_goal_position=(550,30)
 ctc_node=0
 parent_node_index=None
 node_index=0
@@ -94,14 +95,21 @@ open_list=PriorityQueue()
 info=[ctc_node,node_index,parent_node_index,robot_start_position]
 open_list.put(info)
 while True:
+    if(open_list.empty()):
+        print("No solution")
     info=open_list.get()
     if(info[3]==robot_goal_position):
-        print("goal reached")
+        print("goalt reached")
         closed_list[info[1]]=[info[0],info[2],info[3]]
         print(closed_list[info[2]])
         goal_node=info[1]
         break
     ctc_move,new_pos=robot_move_left(info[3])
+    if(new_pos==robot_goal_position):
+        print("goal reached")
+        closed_list[node_index+1]=[ctc_move+info[0],info[2],new_pos]
+        goal_node=node_index+1
+        break
     if screen.get_at(new_pos) == white:
         if not any(lst[2] == new_pos for lst in closed_list.values()):
             if not any(items[3] == new_pos for items in open_list.queue):
@@ -120,6 +128,11 @@ while True:
 
 
     ctc_move,new_pos=robot_move_up(info[3])
+    if(new_pos==robot_goal_position):
+        print("goal reached")
+        closed_list[node_index+1]=[ctc_move+info[0],info[2],new_pos]
+        goal_node=node_index+1
+        break
     if screen.get_at(new_pos) == white:
         if not any(lst[2] == new_pos for lst in closed_list.values()):
             if not any(items[3] == new_pos for items in open_list.queue):
@@ -135,6 +148,11 @@ while True:
                             
                             
     ctc_move,new_pos=robot_move_right(info[3])
+    if(new_pos==robot_goal_position):
+        print("goal reached")
+        closed_list[node_index+1]=[ctc_move+info[0],info[2],new_pos]
+        goal_node=node_index+1
+        break
     if screen.get_at(new_pos) == white:
         if not any(lst[2] == new_pos for lst in closed_list.values()):
             if not any(items[3] == new_pos for items in open_list.queue):
@@ -151,6 +169,11 @@ while True:
                             
 
     ctc_move,new_pos=robot_move_down(info[3])
+    if(new_pos==robot_goal_position):
+        print("goal reached")
+        closed_list[node_index+1]=[ctc_move+info[0],info[2],new_pos]
+        goal_node=node_index+1
+        break
     if screen.get_at(new_pos) == white:
         if not any(lst[2] == new_pos for lst in closed_list.values()):
             if not any(items[3] == new_pos for items in open_list.queue):
@@ -166,6 +189,11 @@ while True:
                             
     
     ctc_move,new_pos=robot_move_down_left(info[3])
+    if(new_pos==robot_goal_position):
+        print("goal reached")
+        closed_list[node_index+1]=[ctc_move+info[0],info[2],new_pos]
+        goal_node=node_index+1
+        break
     if screen.get_at(new_pos) == white:
         if not any(lst[2] == new_pos for lst in closed_list.values()):
             if not any(items[3] == new_pos for items in open_list.queue):
@@ -181,6 +209,11 @@ while True:
                             
     
     ctc_move,new_pos=robot_move_down_right(info[3])
+    if(new_pos==robot_goal_position):
+        print("goal reached")
+        closed_list[node_index+1]=[ctc_move+info[0],info[2],new_pos]
+        goal_node=node_index+1
+        break
     if screen.get_at(new_pos) == white:
         if not any(lst[2] == new_pos for lst in closed_list.values()):
             if not any(items[3] == new_pos for items in open_list.queue):
@@ -196,6 +229,11 @@ while True:
                             
     
     ctc_move,new_pos=robot_move_up_left(info[3])
+    if(new_pos==robot_goal_position):
+        print("goal reached")
+        closed_list[node_index+1]=[ctc_move+info[0],info[2],new_pos]
+        goal_node=node_index+1
+        break
     if screen.get_at(new_pos) == white:
         if not any(lst[2] == new_pos for lst in closed_list.values()):
             if not any(items[3] == new_pos for items in open_list.queue):
@@ -211,6 +249,11 @@ while True:
                             
 
     ctc_move,new_pos=robot_move_up_right(info[3])
+    if(new_pos==robot_goal_position):
+        print("goal reached")
+        closed_list[node_index+1]=[ctc_move+info[0],info[2],new_pos]
+        goal_node=node_index+1
+        break
     if screen.get_at(new_pos) == white:
         if not any(lst[2] == new_pos for lst in closed_list.values()):
             if not any(items[3] == new_pos for items in open_list.queue):
@@ -230,6 +273,8 @@ while True:
     pygame.display.update()
     print(info[2])
 
+    
+
 
 
 print(closed_list)
@@ -241,7 +286,11 @@ current_node = goal_node
 while goal_node is not None:
     goal_node_parent = closed_list[goal_node][1]
     path.append(closed_list[goal_node][2])
+    screen.set_at(closed_list[goal_node][2], green)
     goal_node=goal_node_parent
+    pygame.display.update()
+    
+
     
     
 
