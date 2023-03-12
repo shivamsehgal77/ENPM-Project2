0fcc3711b1a8bc3d59e5bf32a87d49f0578e7605
Shivam <ssehgal7@umd.edu>
2023-03-11 23:51:31 -0500

    seventh commit

--- dijkstra_shivam_sehgal.py
+++ dijkstra_shivam_sehgal.py
@@ -13,14 +13,11 @@ white=(255,255,255)
 
 red = (255,0,0)
 green = (0,255,0)
-# points_hexgon = [(443.30, 125),(421.65, 164.33),(378.35, 164.33),(356.70, 125),(378.35, 85.67),(421.65, 85.67)]
 points_triangle = [(460, 25), (460, 225), (510, 125)]
 points_hexgon = [(300,44.22649731),(230.04809472,84.61324865),(230.04809472,165.38675135),(300,205.77350269),(369.95190528,165.38675135),(369.95190528,84.61324865)]
 points_triangle_border = [(455,246.18033989),(455.00,3.81966011),(515.59016994,125)]
 
 # Set the dimensions of the screen
-# screen = pygame.display.set_mode((600, 250))
-# screen = pygame.display.set_mode((600, 250), pygame.NOFRAME)
 screen = pygame.Surface((600, 250))
 pygame.draw.rect(screen, white, pygame.Rect(5,5,590,240))
 pygame.draw.rect(screen,red,pygame.Rect(95,5,60,105))
